a04830648ff23a27c8b6c897edd4d00e2f4e6251
rvance1 <roystonvance@gmail.com>
2026-05-09 13:10:49 -0600

    taq dao basic complete

--- src/taq_backtester/dal/dao/taq_dao.py
+++ src/taq_backtester/dal/dao/taq_dao.py
@@ -5,6 +5,7 @@ import polars as pl
 
 from taq_backtester.dal.models.database import Database
 from taq_backtester.dal.models.taq_table import TaqTable, TaqType
+from taq_backtester.dal.models.schema import QuoteSchema, QuoteDf, TradeSchema, TradeDf
 
 class TaqDao(BaseModel):
     database: Database
@@ -12,9 +13,26 @@ class TaqDao(BaseModel):
     def get_table(self) -> TaqTable:
         return self.database.get_taq_table()
     
-    def load_by_date(self, date: dt.date, type: TaqType) -> pl.DataFrame:
+    def load_quote_by_date(self, date: dt.date) -> QuoteDf:
         table = self.get_table()
-        return table.scan_date(date, type).collect()
+        return QuoteSchema.validate(table.scan_date(date, TaqType.QUOTE).collect())
     
-    def load_by_range() -> pl.DataFrame:
-        pass
+    def load_quote_by_range(self, start_date: dt.datetime, end_date: dt.datetime) -> QuoteDf:
+        table = self.get_table()
+        return QuoteSchema.validate(
+            table.scan_range(start_date, end_date, TaqType.QUOTE)
+            .filter(pl.col("datetime").is_between(start_date, end_date))
+            .collect()
+        )
+    
+    def load_trade_by_date(self, date: dt.date) -> TradeDf:
+        table = self.get_table()
+        return TradeSchema.validate(table.scan_date(date, TaqType.TRADE).collect())
+    
+    def load_trade_by_range(self, start_date: dt.datetime, end_date: dt.datetime) -> TradeDf:
+        table = self.get_table()
+        return TradeSchema.validate(
+            table.scan_range(start_date, end_date, TaqType.TRADE)
+            .filter(pl.col("datetime").is_between(start_date, end_date))
+            .collect()
+        )

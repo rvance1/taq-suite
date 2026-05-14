cdd712a87574522084c5b6737649a9aee6b245fe
rvance1 <roystonvance@gmail.com>
2026-05-13 19:31:33 -0600

    added generate orders function

--- src/taq_backtester/engine/backtester.py
+++ src/taq_backtester/engine/backtester.py
@@ -6,7 +6,7 @@ from .models.schema import WeightsSchema, WeightsDf, SharesSchema, SharesDf
 from taq_backtester.dal.dao.taq_dao import TaqDao
 from taq_backtester.dal.models.schema import QuoteHistorySchema, QuoteHistoryDf
 
-from .computations import compute_prices, compute_aum, compute_optimal_shares
+from .computations import compute_prices, compute_aum, compute_optimal_shares, compute_delta_shares
 
 class Backtester():
     def __init__(self, config: BTConfig, taq_dao: TaqDao):
@@ -14,22 +14,34 @@ class Backtester():
         self.taq_dao = taq_dao
 
         self.current_date = self.config.start_date
-        self.holdings: SharesDf = SharesDf.empty(schema=SharesSchema)
+        self.holdings: SharesDf = pl.DataFrame()
         self.cash = self.config.initial_aum
-
-    def get_aum(self, quote_data_day: QuoteHistoryDf) -> float:
         
-        
-
-    def make_orders_on_date(self, date, optimal_weights: WeightsDf):
-        weights = optimal_weights.filter(pl.col("datetime") == date)
+    def generate_orders(self, date, optimal_weights: WeightsDf):
         taq = self.taq_dao.load_quote_by_date(date)
         prices = compute_prices(taq)
+
+        aum = compute_aum(
+            holdings=self.holdings,
+            prices=prices,
+            cash=self.cash
+        )
+
+        optimal_shares = compute_optimal_shares(
+            optimal_weights=optimal_weights,
+            prices=prices,
+            aum=aum
+        )
+
+        delta_shares = compute_delta_shares(
+            optimal_shares=optimal_shares,
+            current_shares=self.holdings
+        )
 
         # optimal weights -> optimal shares
         # optimal shares - current shares = orders
         # execute orders
-        
+        pass
         
 
 
